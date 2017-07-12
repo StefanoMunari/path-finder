251131a3d1823c0484d8756b656d523ea6c4cdd1
StefanoMunari <stefanomunari.sm@gmail.com>
2017-07-12 23:16:58 +0200

    improve reader

--- src/io/reader.py
+++ src/io/reader.py
@@ -3,13 +3,19 @@ import json
 
 def get_dictionary_from_file(file_path):
   dictionary = dict();
-  with open(file_path) as json_data:
-      json_data = json.load(json_data);
-      for key in json_data.keys():
-        dictionary[key] = list();
-        for element in json_data[str(key)]:
-        	if isinstance(element, int):
-	          dictionary[key].append(int(element));
-        	else:
-	          dictionary[key].append(element);
+  file = open(file_path, "r");
+  try:
+    with file as json_data:
+        json_data = json.load(json_data);
+        for key in json_data.keys():
+          dictionary[key] = list();
+          for element in json_data[str(key)]:
+            if isinstance(element, int):
+              dictionary[key].append(int(element));
+            else:
+              dictionary[key].append(str(element));
+  except Exception as e:
+    print(traceback.format_exception(*sys.exc_info()));
+  finally:
+    file.close();
   return dictionary;
