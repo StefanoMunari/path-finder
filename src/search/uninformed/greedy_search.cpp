3a672b3961588f8be0e2f5bbe5364ae893fd648f
StefanoMunari <stefanomunari.sm@gmail.com>
2017-07-15 12:39:40 +0200

    update usage of MakeMap, MakeSearch and MakeMapDestructor; convert comment style

--- src/search/uninformed/greedy_search.cpp
+++ src/search/uninformed/greedy_search.cpp
@@ -44,39 +44,66 @@ GreedySearch<State>::Search(GraphPtr_IdMap static_graph_,
 	shared_ptr<GraphPtr_IdMap> dynamic_graph_,
 	const Problem<State>& problem){
 
-	/* boost-property accessors */
+	// boost-property accessors
 	IndexMap node_index =
 		boost::get(boost::vertex_index, (*static_graph_.first));
-	/* define variables */
+	// shortcut for verbose type
+	typedef std::pair<NodeColored<State>*, uint> SearchableColoredNode;
+	// define variables
 	Graph *static_graph = static_graph_.first;
 	Graph *dynamic_graph = (dynamic_graph_.get())->first;
+	// the resulting path is empty (no solution found yet)
+	list<State>* result = EMPTY;
+	// state -> vertex_index
 	map<string, int>* indexes_map = (map<string, int>*)static_graph_.second;
+	// vertex_index -> state
 	auto ids_map = Algorithm::GetReversedMap<string, int>(indexes_map);
-	auto contour = this->_qmaker.MakeQueue(problem.GetFirstState());
+	// set-up contour
+	auto contour = this->_qmaker.MakeQueue();
+	// push the source node and mark it as completed/explored
+	contour->push(
+		SearchableColoredNode(
+			new NodeColored<State>(problem.GetFirstState(), nullptr, BLACK),
+			0));
+	// set-up search map
 	auto search_map =
-		this->_search_map_maker.MakeSearchMap(
-			(contour->top()).first->state, &ids_map, static_graph_.first);
+		this->_search_map_maker.MakeMap(&ids_map, static_graph_.first);
+	// remove the deprecated source,
+	// insert the source node marking it as BLACK (visited/completed)
+	{
+		auto deprecated_node = (*search_map)[problem.GetFirstState()];
+		delete deprecated_node.first;
+	}
+	(*search_map)[problem.GetFirstState()] =
+							SearchableColoredNode(
+								new NodeColored<State>(
+									problem.GetFirstState(), nullptr, BLACK),0);
+	// declare iterators
 	VertexDescriptor current, end;
 	end = boost::graph_traits<Graph>::null_vertex();
 
-	/* body */
+	// algorithm
 	while(!contour->empty() && (current != end)){
 		auto current_node = contour->top();
 		contour->pop();
 		current = (*indexes_map)[current_node.first->state];
-		/* goal reached */
-		if(problem.IsGoal(current_node.first->state))
-			return Solve(current_node.first);
-		/* explore the current area */
+		// goal reached
+		if(problem.IsGoal(current_node.first->state)){
+			result = Solve(current_node.first);
+			break;
+		}
+		// explore the current area
 		auto neighbors =
 			boost::adjacent_vertices(current, *static_graph_.first);
 
 		for(auto n_it = neighbors.first; n_it !=  neighbors.second; ++n_it){
 			string neighbor = ids_map[node_index[*n_it]];
 			auto current_neigh = (*search_map)[neighbor];
-			/* node not yet explored */
+			// node not yet explored
+			// if a node already exists in the priority queue, then do not
+			// insert it another time but keep the first found
 			if(current_neigh.first->color  ==  WHITE){
-				/* mark the node as explored */
+				// mark the node as explored
 				current_neigh.first->color = BLACK;
 				auto neigh_cost = current_node.second +
 		   			(*static_graph)
@@ -89,6 +116,7 @@ GreedySearch<State>::Search(GraphPtr_IdMap static_graph_,
 			}
 		}
 	}
-	/* search failed */
-	return EMPTY;
+
+	(this->_search_map_maker.MakeMapDestructor())(search_map);
+	return result;
 }
--- src/search/uninformed/uniform_cost_search.cpp
+++ src/search/uninformed/uniform_cost_search.cpp
@@ -47,27 +47,50 @@ UniformCostSearch<State>::Search(GraphPtr_IdMap static_graph_,
 	/* boost-property accessors */
 	IndexMap node_index =
 		boost::get(boost::vertex_index, (*static_graph_.first));
+	// shortcut for verbose type
+	typedef std::pair<NodeColored<State>*, uint> SearchableColoredNode;
 	/* define variables */
 	Graph *static_graph = static_graph_.first;
 	Graph *dynamic_graph = (dynamic_graph_.get())->first;
+	// the resulting path is empty (no solution found yet)
+	list<State>* result = EMPTY;
+	// state -> vertex_index
 	map<string, int>* indexes_map = (map<string, int>*)static_graph_.second;
+	// vertex_index -> state
 	auto ids_map = Algorithm::GetReversedMap<string, int>(indexes_map);
-	auto contour = this->_qmaker.MakeQueue(problem.GetFirstState());
+	// set-up contour
+	auto contour = this->_qmaker.MakeQueue();
+	// push the source node and mark it as completed/explored
+	contour->push(
+		SearchableColoredNode(
+			new NodeColored<State>(problem.GetFirstState(), nullptr, BLACK),
+			0));
 	auto search_map =
-		this->_search_map_maker.MakeSearchMap(
-			(contour->top()).first->state, &ids_map, static_graph_.first);
+		this->_search_map_maker.MakeMap(&ids_map, static_graph_.first);
+	// remove the deprecated source,
+	// insert the source node marking it as BLACK (visited/completed)
+	{
+		auto deprecated_node = (*search_map)[problem.GetFirstState()];
+		delete deprecated_node.first;
+	}
+	(*search_map)[problem.GetFirstState()] =
+							SearchableColoredNode(
+								new NodeColored<State>(
+									problem.GetFirstState(), nullptr, BLACK),0);
+	// declare iterators
 	VertexDescriptor current, end;
 	end = boost::graph_traits<Graph>::null_vertex();
 
-	/* body */
 	while(!contour->empty() && (current != end)){
 		auto current_node = contour->top();
 		contour->pop();
 		current = (*indexes_map)[current_node.first->state];
-		/* goal reached - search completed*/
-		if(problem.IsGoal(current_node.first->state))
-			return Solve(current_node.first);
-		/* explore the current area */
+		// goal reached - search completed
+		if(problem.IsGoal(current_node.first->state)){
+			result = Solve(current_node.first);
+			break;
+		}
+		// explore the current area
 		current_node.first->color = BLACK;
 		auto neighbors =
 			boost::adjacent_vertices(current, *static_graph_.first);
@@ -80,15 +103,15 @@ UniformCostSearch<State>::Search(GraphPtr_IdMap static_graph_,
 					[boost::edge(current,*n_it,(*static_graph)).first] +
 					(*dynamic_graph)
 					[boost::edge(current,*n_it,(*dynamic_graph)).first];
-			/* neighbor not yet explored */
+			// neighbor not yet explored
 			if(current_neigh.first->color  ==  WHITE){
 				current_neigh.first->color = GRAY;
 		   		current_neigh.first->parent = current_node.first;
 		   		current_neigh.second = neigh_cost;
 		   		contour->push(current_neigh);
 			}
-			/* neighbor has been explored and
-				has an higher cost than the current one */
+			// neighbor has been explored and
+			// has an higher cost than the current one
 			else if((current_neigh.first->color == GRAY) &&
 					(current_neigh.second > neigh_cost)){
 				current_neigh.first->parent = current_node.first;
@@ -96,6 +119,7 @@ UniformCostSearch<State>::Search(GraphPtr_IdMap static_graph_,
 			}
 		}
 	}
-	/* search failed */
-	return EMPTY;
+
+	(this->_search_map_maker.MakeMapDestructor())(search_map);
+	return result;
 }
