0e98b9502b909d393ce11abe1037f32898ef12af
StefanoMunari <stefanomunari.sm@gmail.com>
2017-07-23 12:15:41 +0200

    fix indentation (namespace)

--- src/graph/graph_encoder.h
+++ src/graph/graph_encoder.h
@@ -12,21 +12,23 @@
 
 namespace path_finder
 {
-	class GraphEncoder{
-		public:
-			GraphEncoder() noexcept {};
-			GraphEncoder(const GraphEncoder& that) noexcept {};
-			GraphEncoder& operator=(const GraphEncoder& that) noexcept
-			{
-				return *this;
-			}
-			~GraphEncoder() noexcept {};
-			GraphPtr_IdMap
-				Encode(
-					std::map<std::string, std::vector<std::string>> *,
-					std::map<std::string, std::vector<uint>> *)
-				const;
-	};
+
+class GraphEncoder{
+	public:
+		GraphEncoder() noexcept {};
+		GraphEncoder(const GraphEncoder& that) noexcept {};
+		GraphEncoder& operator=(const GraphEncoder& that) noexcept
+		{
+			return *this;
+		}
+		~GraphEncoder() noexcept {};
+		GraphPtr_IdMap
+		Encode(
+			std::map<std::string, std::vector<std::string>> *,
+			std::map<std::string, std::vector<uint>> *)
+		const;
+};
+
 }
 
 #endif /*GRAPH_ENCODER_H*/
