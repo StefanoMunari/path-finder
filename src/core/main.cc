f1216749f5e9942785428b243f72cb5137af03ab
StefanoMunari <stefanomunari.sm@gmail.com>
2017-09-12 23:52:48 +0200

    print costs and total costs in main - PoC to check algorithm results

--- src/core/main.cc
+++ src/core/main.cc
@@ -6,6 +6,7 @@
  * Detailed description of file.
  */
 #include "ai.h"
+#include "io/json_reader.h"
 #include "search/utils/searchable_type.h"
 #include "utils/constants.h"
 #include <map>
@@ -13,10 +14,33 @@
 #include <list>
 #include <string>
 #include <utility>
+#include <climits> // UINT_MAX
 #include <iostream>
 #include <stdlib.h> // getenv
 #include <stdexcept>
 #include <fstream> // std::ifstream
+
+uint GetStepCost(std::map<std::string, std::vector<std::string>> * topology,
+	std::map<std::string, std::vector<uint>> * costs,
+	std::string prev, std::string node)
+{
+	int neigh_index = -1;
+	uint step_cost = UINT_MAX;
+	if(!prev.empty())
+	{
+		std::vector<std::string> neighs = (*topology)[prev];
+		for(uint i=0; neigh_index == -1 && i < neighs.size(); ++i)
+		{
+			if(neighs[i] == node)
+			{
+				neigh_index = i;
+				std::vector<uint> neigh_costs = (*costs)[prev];
+				step_cost = neigh_costs[neigh_index];
+			}
+		}
+	}
+	return step_cost;
+}
 
 int main(int argc, char **argv){
 
@@ -93,25 +117,78 @@ int main(int argc, char **argv){
 	auto result1 =
 		ai1.FindPath<string>(source, destination, path_finder::ASTAR);
 
-	std::cout<<"==========PRINT RESULT========="<<std::endl;
+	std::map<std::string, std::vector<std::string>> * topology =
+		new std::map<std::string, std::vector<std::string>>();
+	std::map<std::string, std::vector<uint>> * costs =
+		new std::map<std::string, std::vector<uint>>();
+
+	path_finder::JSON_Reader::Read(
+		prefix+data_path+f_name_prefix+"_topology"+f_extension,
+		topology);
+
+	path_finder::JSON_Reader::Read(
+		prefix+data_path+f_name_prefix+"_costs"+f_extension,
+		costs);
+
+	std::cout<<"--------------SEARCH RESULTS--------------"<<std::endl;
 	std::cout<<"==========UNIFORM COST========="<<std::endl;
-	for(auto const& node : (*result)) {
-	    std::cout<< node <<std::endl;
+	{
+		std::string prev = "";
+		uint total_cost = 0;
+		for(auto const& node : (*result))
+		{
+			uint step_cost = GetStepCost(topology, costs, prev, node);
+			if(step_cost != UINT_MAX)
+			{
+				std::cout<< "cost => "<< step_cost <<std::endl;
+				total_cost = total_cost+step_cost;
+			}
+			std::cout<< node <<std::endl;
+			prev = node;
+		}
+		std::cout<< "TOTAL COST : "<< total_cost <<std::endl;
 	}
 
 	std::cout<<"==========GREEDY========="<<std::endl;
-	for(auto const& node : (*result0)) {
-	    std::cout<< node <<std::endl;
+	{
+		std::string prev = "";
+		uint total_cost = 0;
+		for(auto const& node : (*result0)) {
+			uint step_cost = GetStepCost(topology, costs, prev, node);
+			if(step_cost != UINT_MAX)
+			{
+				std::cout<< "cost => "<< step_cost <<std::endl;
+				total_cost = total_cost+step_cost;
+			}
+			std::cout<< node <<std::endl;
+			prev = node;
+		}
+		std::cout<< "TOTAL COST : "<< total_cost <<std::endl;
 	}
 
 	std::cout<<"==========ASTAR========="<<std::endl;
-	for(auto const& node : (*result1)) {
-	    std::cout<< node <<std::endl;
+	{
+		std::string prev = "";
+		uint total_cost = 0;
+		for(auto const& node : (*result1)) {
+			uint step_cost = GetStepCost(topology, costs, prev, node);
+			if(step_cost != UINT_MAX)
+			{
+				std::cout<< "cost => "<< step_cost <<std::endl;
+				total_cost = total_cost+step_cost;
+			}
+			std::cout<< node <<std::endl;
+			prev = node;
+		}
+		std::cout<< "TOTAL COST : "<< total_cost <<std::endl;
 	}
+	std::cout<<"------------------------------------------"<<std::endl;
 
 	delete result;
 	delete result0;
 	delete result1;
+	delete topology;
+	delete costs;
 
 	return 0;
 }
