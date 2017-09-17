3ef00a7733993b91c2619bb129c37c524e26355d
StefanoMunari <stefanomunari.sm@gmail.com>
2017-09-17 12:18:50 +0200

    tidy up utils scripts - generate graphs suite, execute performance-test suite, clean resulting data and generate statistics from them

--- utils/clean_data_set.sh
+++ utils/clean_data_set.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
+# substring to remove
+to_remove=${1:-}
+
+function help
+{
+   echo "Usage: $0 <substring to remove>"
+   echo "   example: $0 -3"
+}
+
+if [ -z ${to_remove} ];
+then
+   echo "[ERROR: missing argument]"
+   help
+   exit 1
+fi
+
+directory=$(echo $to_remove| cut -d'-' -f 2)
+
 cd ../times/
-files=($(ls ready))
-cd ready
+files=($(ls $directory))
+cd $directory
 
 for f in "${files[@]}"
 do
-   sed -i "s/-3//g" $f
-done
-
-cd ..
-files=($(ls results))
-cd results
-for f in "${files[@]}"
-do
-   sed -i "s/-9//g" $f
+   sed -i "s/$to_remove//g" $f
 done
--- utils/data_suite_generator.py
+++ utils/data_suite_generator.py
@@ -7,10 +7,10 @@ if(len(sys.argv) != 6):
    raise ValueError('Invalid parameters')
 
 out_path=str(sys.argv[1])
+branching=int(sys.argv[2])
 start=int(sys.argv[2])
 to=int(sys.argv[3])
 step=int(sys.argv[4])
-branching=int(sys.argv[5])
 data=[]
 it=1
 value=start
@@ -20,9 +20,9 @@ while(value < to):
    value=value*step**it
    it=it+1
 
-data=[]
+# data=[]
 
-data=[500, 1000, 1500, 2500, 3500, 4500, 5500, 7000, 8000, 10000, 30000, 50000, 70000, 85000,  100000]
+# data=[500, 1000, 1500, 2500, 3500, 4500, 5500, 7000, 8000, 10000, 30000, 50000, 70000, 85000, 100000, 200000, 400000]
 
 for d in data:
    os.system("python graph_generator.py "+out_path+" "+str(d)+" "+str(branching))
